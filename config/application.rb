33d07ee18e7afe806306442a29190a9892479123
VicDario <26585356+VicDario@users.noreply.github.com>
2024-11-17 11:04:34 -0300

    feat: load env file

--- config/application.rb
+++ config/application.rb
@@ -13,10 +13,18 @@ require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
 # require "rails/test_unit/railtie"
+require 'dotenv/load'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
+=begin
+if Rails.env.development? || Rails.env.test?
+  Dotenv::Rails.load
+end
+=end
+Dotenv::Rails.load
 
 module VehicleMapTest
   class Application < Rails::Application
