2aedceb47fdd94a680e21d5845cc663d86c3b829
VicDario <26585356+VicDario@users.noreply.github.com>
2024-11-22 01:20:13 -0300

    style: format

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,7 @@ Rails.application.routes.draw do
   # root "posts#index"
   namespace :api do
     namespace :v1 do
-      resources :gps, only: [:create]
+      resources :gps, only: [ :create ]
     end
   end
-
 end
