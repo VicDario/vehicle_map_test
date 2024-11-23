2e264ae0d10229fd6bc69196b6b55534170d7045
VicDario <26585356+VicDario@users.noreply.github.com>
2024-11-23 12:19:36 -0300

    fix: pass values correctly

--- app/sidekiq/save_gps_waypoint_job.rb
+++ app/sidekiq/save_gps_waypoint_job.rb
@@ -1,13 +1,16 @@
+require 'json'
+
 class SaveGpsWaypointJob
     include Sidekiq::Job
 
     def perform(params)
         Rails.logger.info("Waypoint queue")
         begin
-            vehicle = SaveVehicleUseCase.execute(params[:vehicle_identifier])
-            SaveWaypointUseCase.execute(params, vehicle)
+            waypoint_params = JSON.parse(params, symbolize_names: true)
+            vehicle = SaveVehicleUseCase.execute(waypoint_params[:vehicle_identifier])
+            SaveWaypointUseCase.execute(waypoint_params, vehicle)
 
-            Rails.logger.info("Waypoint saved successfully for vehicle #{params[:vehicle_identifier]}")
+            Rails.logger.info("Waypoint saved successfully for vehicle #{waypoint_params[:vehicle_identifier]}")
         rescue ArgumentError => e
             Rails.logger.error("Failed to save waypoint: #{e.message}")
         end
