6e45edebeb6d5819ffb8c664bad38cde94041962
VicDario <26585356+VicDario@users.noreply.github.com>
2024-11-23 12:20:16 -0300

    fix: call correctly validate and pass params

--- app/use_cases/save_waypoint_use_case.rb
+++ app/use_cases/save_waypoint_use_case.rb
@@ -1,18 +1,18 @@
 class SaveWaypointUseCase
     def self.execute(params, vehicle)
-        unless valid_latitude?(params[:latitude]) && valid_longitude(params[:longitude])
+        unless valid_latitude?(params[:latitude]) && valid_longitude?(params[:longitude])
             raise ArgumentError, "Invalid latitude or longitude values"
         end
-        WaypointRepository.create_waypoint(params.merge(vehicle_id: vehicle.id))
+        WaypointRepository.create_waypoint(params, vehicle)
     end
 
     private
 
-    def valid_latitude?(latitude)
+    def self.valid_latitude?(latitude)
         latitude.is_a?(Numeric) && latitude >= -90 && latitude <= 90
     end
 
-    def valid_longitude?(longitude)
+    def self.valid_longitude?(longitude)
         longitude.is_a?(Numeric) && longitude >= -180 && longitude <= 180
     end
 end
