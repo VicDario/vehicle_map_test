bc79c3a3395139185cc3537d755699f5e7cee448
VicDario <26585356+VicDario@users.noreply.github.com>
2024-11-23 12:23:00 -0300

    feat: add validator for identifier

--- app/use_cases/save_vehicle_use_case.rb
+++ app/use_cases/save_vehicle_use_case.rb
@@ -1,5 +1,14 @@
 class SaveVehicleUseCase
     def self.execute(vehicle_identifier)
+        unless SaveVehicleUseCase.valid_identifier?(vehicle_identifier)
+            raise ArgumentError, "Invalid vehicle identifier"
+        end
         VehicleRepository.find_or_create(vehicle_identifier)
     end
+
+    private
+
+    def self.valid_identifier?(vehicle_identifier)
+        vehicle_identifier.is_a?(String) && vehicle_identifier.length > 3 && vehicle_identifier.length < 10
+      end
 end
