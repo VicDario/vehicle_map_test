b84a17ac4aa761f2036026f9ae095ea504d34f21
VicDario <26585356+VicDario@users.noreply.github.com>
2024-11-25 18:09:43 -0300

    feat: get latest vehicles waypoints

--- app/repositories/waypoint_repository.rb
+++ app/repositories/waypoint_repository.rb
@@ -1,5 +1,13 @@
 class WaypointRepository
     def self.create_waypoint(params, vehicle)
-      Waypoint.create!(latitude: params[:latitude], longitude: params[:longitude], sent_at: params[:sent_at], vehicle: vehicle)
+        Waypoint.create!(latitude: params[:latitude], longitude: params[:longitude], sent_at: params[:sent_at], vehicle: vehicle)
+    end
+
+    def self.get_latest_waypoint_of_each_vehicle
+        waypoints = Waypoint.select("DISTINCT ON (vehicle_id) vehicle_id as vehicle_identifier, longitude, latitude, sent_at")
+                .order(:vehicle_id, created_at: :desc)
+        waypoints.map do |waypoint|
+            waypoint.attributes.except("id")
+        end
     end
 end
