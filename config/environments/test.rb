a81341312101094b95a0d6985316732fb3833ce0
VicDario <26585356+VicDario@users.noreply.github.com>
2024-11-26 11:46:32 -0300

    tets: add sidekiq for testing

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -50,4 +50,8 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # Sidekiq config
+  # config.active_job.queue_adapter = :test
+  config.active_job.queue_adapter = :sidekiq
 end
